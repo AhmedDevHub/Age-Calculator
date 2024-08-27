26e4a70dab5eae4d128cbe5968bc603729240874
Ahmed Ashraf <ahmed.ashraf0221@gmail.com>
2024-08-27 16:18:05 +0300

    Age Calculator
    Simple App using HTML, CSS, Bootstrap, JS

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// Lottie animation loading
+var animation = lottie.loadAnimation({
+    container: document.getElementById('animation'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '/img/Animation - 1724603548727.json' // Actual path to your JSON file
+});
+
+function calculateAge() {
+    const dobInput = document.getElementById('dob').value;
+    const dob = new Date(dobInput);
+    const today = new Date();
+
+    if (dobInput === '') {
+        document.getElementById('result').innerText = "Please select a date of birth.";
+        return;
+    }
+
+    let years = today.getFullYear() - dob.getFullYear();
+    let months = today.getMonth() - dob.getMonth();
+    let days = today.getDate() - dob.getDate();
+
+    // Adjust if the current date is before the birth date in the current year
+    if (days < 0) {
+        months--;
+        days += new Date(today.getFullYear(), today.getMonth(), 0).getDate(); // days in previous month
+    }
+
+    if (months < 0) {
+        years--;
+        months += 12;
+    }
+
+    document.getElementById('result').innerHTML = `Hey!! You are <span>${years}</span> years, <span>${months}</span> months, and <span>${days}</span> days old.`;
+}
+
